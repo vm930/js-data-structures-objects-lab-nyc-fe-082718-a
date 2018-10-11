dada9006e9c9acf7c497759197bd7b2d101c41b3
abstract-animation-9914 <vm930@hotmail.com>
2018-10-11 00:38:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
 
 function deleteFromDriverByKey(driver, key) {
   const newDriver = delete driver.key;
+  return newDriver;
 }
 
 //destructivelyDeleteFromDriverByKey(driver, key)
