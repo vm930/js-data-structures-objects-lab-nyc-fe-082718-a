6f60b499cdaf158540f2293d6f6562d2247f1258
abstract-animation-9914 <vm930@hotmail.com>
2018-10-11 00:52:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const driver = { name: "Sam"};
+//‣updateDriverWithKeyAndValue(driver, key, value)
+function updateDriverWithKeyAndValue(driver, key, value){
+ const newDriver = Object.assign({}, driver, {[key]: value});
+ return newDriver;
+}
+//
+
+//destructivelyUpdateDriverWithKeyAndValue(driver, key, value)
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+   return Object.assign(driver,{[key]:value});
+}
+
+//deleteFromDriverByKey(driver, key)
+
+function deleteFromDriverByKey(driver, key) {
+  const newDriver = delete driver.key;
+  return newDriver;
+}
+
+//destructivelyDeleteFromDriverByKey(driver, key)
+
+function destructivelyDeleteFromDriverByKey(driver, key){
+  return delete driver[key];
+}
