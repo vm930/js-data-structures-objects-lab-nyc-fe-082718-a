cc664cb6a062728e2841c4ee99b8ce6b591b62f6
abstract-animation-9914 <vm930@hotmail.com>
2018-10-11 00:20:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your solution in this file!
+const driver = { name: "Sam"};
+//‣updateDriverWithKeyAndValue(driver, key, value)
+function updateDriverWithKeyAndValue(driver, key, value){
+ const newDriver = Object.assign({}, driver, {[key]: value});
+ return newDriver;
+}
+//
